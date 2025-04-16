9837cfce71002473c488620afe75cbbbfba8b2fe
aleotoidayy <luannguyen6695@gmail.com>
2025-04-16 23:38:15 +0700

    Delete mayy.js

--- mayy.js
+++
@@ -1,9 +0,0 @@
-// AleooxMayy Auto Hello Script
-
-let message = {
-  title: "AleooxMayy",
-  content: "Hello Bé Mây iu của Lẹo~",
-  icon: "heart.circle",
-};
-
-$done({ title: message.title, message: message.content, icon: message.icon });
