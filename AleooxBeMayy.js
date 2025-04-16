8446a6490ee5afcc88f4934a9da8b160e42e9002
aleotoidayy <luannguyen6695@gmail.com>
2025-04-16 13:09:57 +0700

    aleoo

---
+++ AleooxBeMayy.js
@@ -0,0 +1,11 @@
+#!name= CapCut Premium
+#!desc= Unlock by Lẹo x Mây
+#!author= 𝗔𝗹𝗲𝗼𝗼𝘅𝗠𝗮𝘆𝘆
+#!icon= https://link.icon.capcut.png
+
+[Script]
+[Script]
+capcut-vip = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), requires-body=true, max-size=-1, script-path=https://raw.githubusercontent.com/aleotoidayy/ALeoxBeMayy/main/𝗔𝗹𝗲𝗼𝗼𝘅𝗕𝗲𝗠𝗮𝘆.js
+
+[MITM]
+hostname = %APPEND% api.revenuecat.com
