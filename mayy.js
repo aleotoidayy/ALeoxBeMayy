5747484df47155543051391bcd0db1045d95436a
aleotoidayy <luannguyen6695@gmail.com>
2025-04-16 22:50:46 +0700

    aleoo

---
+++ mayy.js
@@ -0,0 +1,9 @@
+// AleooxMayy Auto Hello Script
+
+let message = {
+  title: "AleooxMayy",
+  content: "Hello Bé Mây iu của Lẹo~",
+  icon: "heart.circle",
+};
+
+$done({ title: message.title, message: message.content, icon: message.icon });
